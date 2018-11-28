33c02eaf3bb806523c10c1a2e9895198cf32d494
allnulled <33976587+allnulled@users.noreply.github.com>
2018-11-28 20:28:16 +0100

    The "20" at the begining removed

--- javascript-1-line-dateformat.js
+++ javascript-1-line-dateformat.js
@@ -1,5 +1,4 @@
-"20" 
-+ (d.getFullYear()-1) + "/" 
+(d.getFullYear()-1) + "/" 
 + (((d.getMonth()+1) < 10) ? "0" + (d.getMonth()+1) : (d.getMonth()+1)) + "/" 
 + (((d.getDate()) < 10) ? "0" + (d.getDate()) : (d.getDate())) + " " 
 + (((d.getHours()) < 10) ? "0" + (d.getHours()) : (d.getHours())) + ":" 
