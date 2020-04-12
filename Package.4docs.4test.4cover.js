a4f897f0eca278cc97d9189ef66a23f024331b00
allnulled <33976587+allnulled@users.noreply.github.com>
2020-04-12 02:46:08 +0200

    Added test and coverage support

--- Package.4docs.4test.4cover.js
+++ Package.4docs.4test.4cover.js
@@ -2,22 +2,31 @@
 //////////////// package.json ////////////////
 //////////////////////////////////////////////
 
-// {
-//   "scripts": {
+// >>> scripts:
     "docs": "npm run docs:diagrams && npm run docs:code",
     "docs:diagrams": "node dev/build.docs.diagrams.js",
     "docs:code": "javadoc -i ./src/**/*/.js ./src/**/*/.vue ./src/**/*/.scss ./src/**/*/.css -o docs/README.api.md -f markdown && concat docs/README.ini.md docs/README.api.md docs/README.end.md",
-    "dev:diagrams": "chokidar ./docs/**/*.skm -c 'npm run docs'"
-//   },
-//   "devDependencies": {
+    "dev:diagrams": "chokidar ./docs/**/*.skm -c 'npm run docs'",
+    "dev": "nodemon -w ./src -w ./test --ext '.js,.ejs' -x 'npm run test'",
+    "build": "npm run docs && npm shrinkwrap",
+    "upload": "git add . && git commit && git push",
+    "test": "mocha --sort --bail test/*.test.js",
+    "cover": "nyc --reporter=html mocha --sort --bail test/*.test.js"
+// <<< :scripts
+
+// >>> devDependencies:
+    "chai": "^4.2.0",
+    "chokidar-cli": "^2.1.0",
     "concat": "^1.0.3",
     "execute-command-sync": "^1.0.2",
     "javadoc": "^2.0.0",
     "mermaid.cli": "^0.5.1",
+    "mocha": "^7.1.1",
     "node-plantuml": "^0.9.0",
+    "nodemon": "^2.0.3",
+    "nyc": "^15.0.1",
     "skemator": "^1.0.3"
-//   }
-// }
+// <<< :devDependencies
 
 ///////////////////////////////////////////////////////
 //////////////// dev/build.diagrams.js ////////////////
