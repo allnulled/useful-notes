25b730786dd36c223201c53b2070bca847a989bf
allnulled <33976587+allnulled@users.noreply.github.com>
2020-06-18 15:05:10 +0200

    Create files-to-command-function.js

---
+++ files-to-command-function.js
@@ -0,0 +1,12 @@
+module.exports = function filesToCommand(text) {
+  return text
+    .split(/\n[\t\r\n ]*/gi)
+    .filter(s => s !== "")
+    .map(f => {
+      const isFile = f.endsWith("#");
+      const command = isFile ? "touch" : "mkdir";
+      const file = isFile ? f.substr(0, f.length-2) : f;
+      return `${command} ${file}`;
+    })
+    .join("\n");
+}
