ff32d3f2a0dbdb70939393a6c50dda2857fa65a0
allnulled <33976587+allnulled@users.noreply.github.com>
2020-06-18 15:06:29 +0200

    Update files-to-command-function.js

--- files-to-command-function.js
+++ files-to-command-function.js
@@ -1,3 +1,22 @@
+/*
+(function filesToCommand(text) {
+  return text
+    .split(/\n[\t\r\n ]*/gi)
+    .filter(s => s !== "")
+    .map(f => {
+      const isFile = f.endsWith("#");
+      const command = isFile ? "touch" : "mkdir";
+      const file = isFile ? f.substr(0, f.length-2) : f;
+      return `${command} ${file}`;
+    })
+    .join("\n");
+})(`
+/a
+/a/b
+/a/b/c
+/a/b/c/README.md
+`)
+//*/
 module.exports = function filesToCommand(text) {
   return text
     .split(/\n[\t\r\n ]*/gi)
